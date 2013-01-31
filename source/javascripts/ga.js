2ad9a42819f3625d9053d883b0663ec9655397b4
GerlandoP <gerlando@gmail.com>
2013-01-30 21:05:48 -0800

    added blog stack post

--- source/javascripts/ga.js
+++ source/javascripts/ga.js
@@ -1,5 +1,5 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-37226863-1']);
+_gaq.push(['_setAccount', 'UA-38117224-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
@@ -7,3 +7,4 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
